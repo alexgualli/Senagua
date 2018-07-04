d7355e75ef849c1c8c56ef81cac23f17d1db6ce0
alexgualli <alex.gualli.ga@gmail.com>
2018-07-04 17:57:23 -0500

    test completo

--- test/TestCliente.java
+++ test/TestCliente.java
@@ -2,7 +2,8 @@
 
 import ec.gob.senagua.entidades.Cliente;
 import ec.gob.senagua.implementacion.ImpCliente;
-import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -22,35 +23,89 @@ public class TestCliente {
     public void test() {
         
         ImpCliente imp = new ImpCliente();
-        Cliente cliente = null;
+        Cliente cliente = new Cliente();
         
-        //cliente= new Cliente(100, "alex", "gualli", "dolorosa", "0604405357", "alex.gualli.ga@gmail.com", new Date(1997, 10, 5), 10);
-        
-        
-        cliente.setCodigo(200);
+        //cliente= new Cliente(100, "alex", "gualli", "UNACH", "0604405357", "alex.gualli.ga@gmail.com", "1997-10-10", 10);
+             
+        cliente.setCodigo(100);
         cliente.setNombre("Sin");
         cliente.setApellido("Cuello");
         cliente.setCedula("0604405357");
         cliente.setCodigoDis(10);
         cliente.setCorreo("sincuello@gmail.com");
         cliente.setDireccion("la dolo");
-        cliente.setFechaNac(new Date(1997, 10, 5));
+        cliente.setFechaNac("2017-2-25");
+        
         
         //INSERTAR 
         int insert = 0;
          try {
              insert=imp.insertar(cliente);
-             if(insert!=0){
-                System.out.println("Insertado: "+cliente.toString());                
+             if(insert>0){
+                System.out.println("INSERTADO: "+cliente.toString());                
             }                      
         } catch (Exception e) {
-            System.err.println("Error al insertar: " + e.getMessage());
+            System.err.println("¡ERROR! ERROR AL INSERTAR: " + e.getMessage());
         }        
         assertTrue(insert>0);
+        
+        //ACTUALIZAR
+        int update = 0;
+         try {
+             cliente.setApellido("SIN APELLIDO");
+             update=imp.actualizar(cliente);
+             if(update>0){
+                System.out.println("ACTUALIZADO: "+cliente.toString());                
+            }                      
+        } catch (Exception e) {
+            System.err.println("¡ERROR! ERROR AL ACTUALIZAR: " + e.getMessage());
+        }        
+         assertTrue(update>0);
+        
+         //LISTAR POR CODIGO
+         Cliente cliente2 = null;
+        try {
+            cliente2 = imp.obtenerCodigo(100);
+            System.out.println("BUSCADO POR CODIGO: " + cliente2.toString());
+        } catch (Exception e) {
+            System.err.println("ERROR! ERROR AL BUSCAR: " + e.getMessage());
+        }
+        assertTrue(cliente2 != null);       
+        
+        //LISTAR TODOS
+        List<Cliente> lista = new ArrayList<>();
+        try {
+            lista = imp.obtenerTodos();
+            System.out.println("\n TODOS LOS CLIENTES");
+            for (Cliente cli : lista) {
+                System.out.println("\t" + cli.toString());
+            }
+        } catch (Exception e) {
+            System.err.println("¡ERROR! NINGUN CLIENTE ENCONTRADO: " +e);
+        }
+        assertTrue(lista.size() >0);
+        
+        
+        
+        //ELIMINAR
+        int delete = 0;
+        try {
+            delete = imp.eliminar(cliente.getCodigo());
+            if (delete!=0) {
+                System.out.println("ELIMINADO: " + cliente.toString());
+            }
+        } catch (Exception e) {
+            System.err.println("¡ERROR! NO SE PUDO ELIMINAR: "+ e);
+        }
+        assertTrue(delete >= 0);
+
+    }
+  
         
         
         
     }
+
     
     
-}
+
