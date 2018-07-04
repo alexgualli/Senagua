38eb1f01be9f3889b457459f122cd206cabbf68e
paul <paul@PANCHO>
2018-07-04 10:34:10 -0500

    pakete DAO.CONTRATO imterfaces de las entedidades se deben implementar

---
+++ src/java/ec/gob/senagua/dao/contrato/IntDeuda.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.gob.senagua.dao.contrato;
+
+/**
+ *
+ * @author paul
+ */
+public interface IntDeuda {
+    
+}
