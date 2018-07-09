1db3406ebc8f063a19015509d9b96a0ed31c3a3b
paul <paul@PANCHO>
2018-07-09 00:32:50 -0500

    Implementacion COMPLETA

--- src/java/ec/gob/senagua/implementacion/ImpMedidor.java
+++ src/java/ec/gob/senagua/implementacion/ImpMedidor.java
@@ -11,6 +11,7 @@ import ec.gob.senagua.dao.contrato.IntMedidor;
 import ec.gob.senagua.entidades.Cliente;
 import ec.gob.senagua.entidades.Medidor;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,35 +19,51 @@ import java.util.List;
  *
  * @author Usuario
  */
-public class ImpMedidor implements IntMedidor{
-    
+public class ImpMedidor implements IntMedidor {
+
     Coneccion con = new Coneccion();
 
     @Override
     public int insertar(Medidor medidor) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int insert = 0;
+        String sql = "INSERT INTO public.medidor(codigo_cliente, tipo_de_medidor)\n"
+                + "    VALUES (?, ?);";
+        List<Parametro> prts = new ArrayList<>();
+        prts.add(new Parametro(1, medidor.getCliente().getCodigo()));
+        prts.add(new Parametro(2, medidor.getTipoMed()));
+        if (medidor.getCodigo() != null) {
+            sql = "INSERT INTO public.medidor(codigo_cliente, tipo_de_medidor, codigo)\n"
+                    + "    VALUES (?, ?, ?);";
+            prts.add(new Parametro(3, medidor.getCodigo()));
+        }
+        try {
+            insert = con.querySet(sql, prts);
+        } catch (Exception e) {
+            throw e;
+        }
+        return insert;
     }
 
     @Override
     public Medidor obtenerCodigo(String id) throws Exception {
         Medidor medidor = null;
-        String sql = "SELECT codigo, codigo_cliente, tipo_de_medidor\n" +
-                     "  FROM medidor "
+        String sql = "SELECT codigo, codigo_cliente, tipo_de_medidor\n"
+                + "  FROM medidor "
                 + "WHERE codigo=?;";
         List<Parametro> prts = new ArrayList<>();
         prts.add(new Parametro(1, id));
         try {
             ResultSet rst = con.queryGet(sql, prts);
-            while(rst.next()){
+            while (rst.next()) {
                 medidor = new Medidor();
                 medidor.setCodigo(rst.getString("codigo"));
                 medidor.setTipoMed(rst.getString("tipo_de_medidor"));
                 try {
                     ImpCliente cliente = new ImpCliente();
-                    medidor.setCliente(cliente.obtenerCodigo(rst.getInt("codigo_cliente")));                    
+                    medidor.setCliente(cliente.obtenerCodigo(rst.getInt("codigo_cliente")));
                 } catch (Exception e) {
                     throw e;
-                }              
+                }
             }
         } catch (Exception e) {
             throw e;
@@ -56,18 +73,63 @@ public class ImpMedidor implements IntMedidor{
 
     @Override
     public List<Medidor> obtenerTodos() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Medidor> lista = new ArrayList<>();
+        String sql = "SELECT codigo, codigo_cliente, tipo_de_medidor\n"
+                + "  FROM public.medidor;";
+        try {
+            ResultSet rst = con.queryGet(sql);
+            while (rst.next()) {
+                Medidor medidor = new Medidor();
+                medidor.setCodigo(rst.getString("codigo"));
+                medidor.setTipoMed(rst.getString("tipo_de_medidor"));
+                try {
+                    ImpCliente cliente = new ImpCliente();
+
+                    medidor.setCliente(cliente.obtenerCodigo(rst.getInt("codigo_cliente")));
+                } catch (Exception e) {
+                    throw e;
+                }
+                lista.add(medidor);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            throw e;
+        }
+        return lista;
     }
 
     @Override
     public int actualizar(Medidor medidor) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int update = 0;
+        String sql = "UPDATE public.medidor\n"
+                + "   SET codigo_cliente=?, tipo_de_medidor=?\n"
+                + " WHERE codigo=?;";
+        List<Parametro> prts = new ArrayList<>();
+        prts.add(new Parametro(1, medidor.getCliente().getCodigo()));
+        prts.add(new Parametro(2, medidor.getTipoMed()));
+        prts.add(new Parametro(3, medidor.getCodigo()));
+        try {
+            update = con.querySet(sql, prts);
+        } catch (Exception e) {
+            throw e;
+        }
+        return update;
     }
 
     @Override
-    public int eliminar(int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int eliminar(String id) throws Exception {
+        int delete = 0;
+        String sql = "DELETE FROM public.medidor\n"
+                + " WHERE codigo=?;";
+        List<Parametro> prts = new ArrayList<>();
+        prts.add(new Parametro(1, id));
+        try {
+            delete = con.querySet(sql, prts);
+        } catch (Exception e) {
+            throw e;
+        }
+        return delete;
     }
+
     
-    
+
 }
